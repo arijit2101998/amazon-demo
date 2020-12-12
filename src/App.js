4012536fd2d3f78baa87983e63a37e5b32e41ae1
Arijit Bhowmik <tataitatait86@gmail.com>
2020-12-12 21:36:29 +0530

    cart page basic setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
-import Home from './Home'
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+
+        <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+
+          
+
+        </Switch>
+        
+      </div>
+    </Router>
   );
 }
 
